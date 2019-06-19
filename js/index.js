b8f619d4fe83373fe50a50950e809045080bef3c
Daniel Schneider <daschne8@gmail.com>
2019-06-19 14:38:27 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -49,5 +49,5 @@ function displayCommits() {
         '</li>'
     )
     .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
+  document.getElementById('details').innerHTML = commitsList;
 }
